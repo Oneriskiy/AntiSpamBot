46cd231bbf26b211cd42c7b1841ecdc1d7bdffbc
Oneriiskiy <german.zapisov12@gmail.com>
2025-01-14 23:59:40 +0300

    final

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ dp = Dispatcher()
 bot = Bot(TOKEN)
 router = Router()
 
-
 @router.message(Command("start"))
 async def StartFunc(message: types.Message):
     await message.answer("Привет! Задай вопрос, интересующий тебя здесь\n\n Он обязательно дойдет нужному пользователю! ")
@@ -16,15 +15,21 @@ async def StartFunc(message: types.Message):
 @router.message()
 async def FirstFunc(message: types.Message):
     user_message = message.text
-    builder = InlineKeyboardMarkup(inline_keyboard=[    
-        [InlineKeyboardButton(text="Ответить", callback_data="answer")],
-        ])
-    await bot.send_message(ADMIN_ID,f"Сообщение от Пользователя {message.from_user.full_name} \n\n{user_message}", reply_markup = builder)
-    await message.reply("Сообщение отправлено! Данный бот создан @Oneriiskiy")
+    if (user_message != "Я бот"):
+        builder = InlineKeyboardMarkup(inline_keyboard=[    
+                [InlineKeyboardButton(text="Узнать кто пишет", callback_data="answer")],
+            ])
+        await bot.send_message(ADMIN_ID,f"Сообщение от Пользователя {message.from_user.full_name} \n\n{user_message}", reply_markup = builder)
+        await message.reply("Сообщение отправлено! Данный бот создан @Oneriiskiy")
+    else:  
+        await message.answer(f"{message.from_user.full_name} - Вы Бот")
+     
 
 @router.callback_query(lambda c: c.data == 'answer')
 async def answer(callback_query: types.CallbackQuery):
-    await callback_query.message.text("cedcew")
+    username = callback_query.from_user.username  
+    fullname = callback_query.from_user.full_name  
+    await callback_query.message.edit_text(f"Имя пользователя: @{username} - Его имя - {fullname}")
 
 
 async def main_func():
